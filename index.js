009bdb01576e1a84f0dfd83038dc09ff243f88ab
Ross Westwater <ross.westwater@gmail.com>
2021-12-19 15:40:48 -0700

    added functionality after meeting with TAs in office hours and tutor

--- index.js
+++ index.js
@@ -1,13 +1,139 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown")
+const fs = require("fs")
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "Please enter the title of your project (Required).",
+    validate: (titleInput) => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log("You must enter a project title to continue.");
+        return false;
+      }
+    }
+  },
+  {
+    type: "input",
+    name: "description",
+    message:
+      "Please enter the description details of your desired README (Required).",
+    validate: (descriptionInput) => {
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log(
+          "Please take some time to describe the what/why/how of this project."
+        );
+        return false;
+      }
+    }
+  },
+  {
+    type: "input",
+    name: "instlInstrctns",
+    message:
+      "If your project has any installation instructions to include, please list them here.",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message:
+      "If you created an application or package and would like other developers to contribute it, enter any instructions of use for your application you would like potential contributers to adhere to. The Contributor Covenant is an industry standard, but you can always write your own.",
+  },
+  {
+    type: "input",
+    name: "testInstrctns",
+    message:
+      "Go the extra mile and write tests for your application. Then provide examples on how to run them.",
+  },
+  {
+    type: "list",
+    name: "licenseSlct",
+    message: "please select a license you wish to use for this project",
+    choices: [
+      "Apache License 2.0",
+      "GNU GPLv3",
+      "GNU AGPLv3",
+      "GNU LGPLv3",
+      "MIT",
+      "ISC",
+      "Mozilla Public License 2.0",
+      "Boost Software License 1.0",
+      "The Unlicense",
+    ],
+  },
+  {
+    type: "input",
+    name: "usgInstrctns",
+    message: "Provide instructions and examples for use.",
+  },
+  {
+  type: "input",
+  name: "gitName",
+  message:
+    "Please provide your gitHub username.",
+  },
+  {
+  type: "input",
+  name: "email",
+  message:
+    "Please provide your email address.",
+  },
+];
+
+const addScreenShot = (res) => {
+  if (!res.sc) {
+    res.sc = [];
+  }
+  console.log(res);
+  return inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "confirmScreenShot",
+        message:
+          "Would you like to add a screen shot to your README? You will be prompted for as many screen shots as you wish to add.",
+        default: false,
+      },
+      {
+        type: "input",
+        name: "scrnSht",
+        message:
+          "Please enter the alt text and relative filepath of your screen shot in the following format:   ![alt text](/folder/name_of_file.png)",
+        when: ({ confirmScreenShot }) => confirmScreenShot,
+      },
+    ])
+    .then((screenShotInfo) => {
+      if (screenShotInfo.confirmScreenShot) {
+        res.sc.push(screenShotInfo.scrnSht);
+        return addScreenShot(res);
+      } else {
+        return res;
+      }
+    });
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+  console.log(data);
+  fs.writeFileSync("./dist/README.md", generateMarkdown(data))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions)
+    .then(addScreenShot)
+    .then((res) => {writeToFile(res)
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,146 @@
+function generateScreenshot(data) {
+  const imgArr = []
+  data.forEach( image => {
+    imgArr.push(image)
+  })
+  return imgArr;
+}
+
+
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "Apache License 2.0") {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+    `
+  }
+  else if (license === "GNU GPLv3") {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+    `
+  }
+  else if (license === "GNU AGPLv3") {
+    return `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)
+    `
+  }
+  else if (license === "GNU LGPLv3") {
+    return `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)
+    `
+  }
+  else if (license === "MIT") {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+    `
+  }
+  else if (license === "ISC") {
+    return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+    `
+  }
+  else if (license === "Mozilla Public License 2.0") {
+    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+    `
+  }
+  else if (license === "Boost Software License 1.0") {
+    return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)
+    `
+  }
+  else if (license === "The Unlicense") {
+    return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+    `
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+if (license === "Apache License 2.0") {
+  return `
+  A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+  `
+}
+else if (license === "GNU GPLv3") {
+  return `
+  Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.
+  `
+}
+else if (license === "GNU AGPLv3") {
+  return `
+  Permissions of this strongest copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. When a modified version is used to provide a service over a network, the complete source code of the modified version must be made available.
+  `
+}
+else if (license === "GNU LGPLv3") {
+  return `
+  Permissions of this copyleft license are conditioned on making available complete source code of licensed works and modifications under the same license or the GNU GPLv3. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work through interfaces provided by the licensed work may be distributed under different terms and without source code for the larger work.
+  `
+}
+else if (license === "MIT") {
+  return `
+  A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+  `
+}
+else if (license === "ISC") {
+  return `
+  A permissive license lets people do anything with your code with proper attribution and without warranty. The ISC license is functionally equivalent to the BSD 2-Clause and MIT licenses, removing some language that is no longer necessary.
+  `
+}
+else if (license === "Mozilla Public License 2.0") {
+  return `
+  Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work.
+  `
+}
+else if (license === "Boost Software License 1.0") {
+  return `
+  A simple permissive license only requiring preservation of copyright and license notices for source (and not binary) distribution. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+  `
+}
+else if (license === "The Unlicense") {
+  return `
+  A license with no conditions whatsoever which dedicates works to the public domain. Unlicensed works, modifications, and larger works may be distributed under different terms and without source code.
+  `
+}
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}   ${renderLicenseBadge(data.licenseSlct)}
 
+## Description 
+
+${data.description}
+
+## Table of Contents
+
+If your README is very long, add a table of contents to make it easy for users to find what they need.
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+
+## Installation
+
+${data.instlInstrctns}
+
+## Usage 
+
+${data.usgInstrctns}
+${generateScreenshot(data.sc)}
+
+## License
+
+${data.licenseSlct}: ${renderLicenseSection(data.licenseSlct)}
+
+## Contributing
+
+${data.contribution}
+
+## Tests
+
+${data.testInstrctns}
+
+## Questions
+
+<ul>
+<li><a href="https://github.com/${data.gitName}">My GitHub Profile</a></li>
+<li><a href="mailto:${data.email}">Email Me</a>  </li>
+</ul>
 `;
 }
 
